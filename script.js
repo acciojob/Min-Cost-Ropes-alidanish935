244f2085ecf75547544ab5b2508cfafd5f4f2627
alidanish935 <114473927+alidanish935@users.noreply.github.com>
2024-08-14 18:44:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 function mincost(arr)
 { 
 //write your code here
-	const minHeap = new MinHeap();
-	for(let i=0;i<arr.length;i++){
-		minHeap.push(arr[i]);
-	}
-	while(minHeap.size()>1){
-		let first = minHeap.pop();
-		let sec = minHeap.pop();
-		let cost += first+sec;
-		minHeap.push(cost);
-	}
-	minHeap.pop();
+	// const minHeap = new MinHeap();
+	// for(let i=0;i<arr.length;i++){
+	// 	minHeap.push(arr[i]);
+	// }
+	// while(minHeap.size()>1){
+	// 	let first = minHeap.pop();
+	// 	let sec = minHeap.pop();
+	// 	let cost += first+sec;
+	// 	minHeap.push(cost);
+	// }
+	// minHeap.pop();
+
+
+	 let totalCost = 0;
+
+    // Continue until we have only one rope left
+    while (arr.length > 1) {
+        // Sort the array to find the two smallest elements
+        arr.sort((a, b) => a - b);
+
+        // Remove the two smallest ropes from the array
+        let first = arr.shift();
+        let second = arr.shift();
+
+        // Combine the two ropes
+        let cost = first + second;
+        totalCost += cost;
+
+        // Add the combined rope back to the array
+        arr.push(cost);
+    }
+
+    return totalCost;
 // return the min cost
   
 }
